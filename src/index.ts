e717054bbf9d761d4480f8c032ad624ad865d8cf
Paul Schifferer <paul@schifferers.net>
2025-05-29 07:20:17 -0700

    Rename entry

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 //! horology-core.js
-//! version: 0.0.1
+//! version: 0.0.x
 //! authors: Paul Schifferer
 //! license: MIT
 //! horology.pilgrimagesoftware.com
-
-import { hooks as horology } from './lib/utils/hooks';
 
 import { adjust } from './lib/adjust';
 import { compare } from './lib/compare';
 import { convert } from './lib/convert';
 
+export const horology = Object.create({
+    adjust,
+    compare,
+    convert,
+});
+
 horology.adjust = adjust;
 horology.compare = compare;
 horology.convert = convert;
-
-export default horology;
